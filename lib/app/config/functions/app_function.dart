9ffef6a3ae9746df0c9bf132dbbb2f9711528229
aittataabderrahim <aittata.abderrahim@gmail.com>
2022-03-11 21:24:16 +0100

    commit

--- lib/app/config/functions/app_function.dart
+++ lib/app/config/functions/app_function.dart
@@ -1,9 +1,22 @@
 import 'package:flutter/services.dart';
 
+import '../themes/app_theme.dart';
+
 class AppFunction {
   AppFunction._();
 
-  static configureDependencies() {
-    SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp, DeviceOrientation.portraitDown]);
+  static get configureDependencies {
+    SystemChrome.setEnabledSystemUIMode(SystemUiMode.edgeToEdge, overlays: []);
+    SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
+    SystemChrome.setSystemUIOverlayStyle(
+      const SystemUiOverlayStyle(
+        systemNavigationBarColor: AppTheme.transparentColor,
+        systemNavigationBarDividerColor: AppTheme.transparentColor,
+        systemNavigationBarIconBrightness: Brightness.dark,
+        statusBarColor: AppTheme.transparentColor,
+        statusBarIconBrightness: Brightness.dark,
+        statusBarBrightness: Brightness.dark,
+      ),
+    );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
+import 'app/config/functions/app_function.dart';
 import 'app/routes/app_pages.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  AppFunction.configureDependencies();
   runApp(const Foodie());
 }
 
