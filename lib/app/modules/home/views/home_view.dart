1d00dc81f65d4970da4168742adb33ab209872b5
aittataabderrahim <aittata.abderrahim@gmail.com>
2022-04-16 01:01:44 +0000

    commit

--- lib/app/modules/home/views/home_view.dart
+++ lib/app/modules/home/views/home_view.dart
@@ -1,4 +1,3 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
@@ -21,21 +20,7 @@ class HomeView extends GetView<HomeController> {
       ),
       floatingActionButton: FloatingActionButton(
         child: Icon(CupertinoIcons.add),
-        onPressed: () async {
-          print("hello");
-          final FirebaseFirestore firestore = FirebaseFirestore.instance;
-          final String _movies = "movies";
-          late CollectionReference _collectionMovies = firestore.collection(_movies);
-          final String id = _collectionMovies.doc().id;
-          final response = await _collectionMovies.doc(id).set({
-            'id': id,
-            'name': "Game of thrones",
-            'phone_number': "8 Seasons",
-          }).then((_) async {
-            return true;
-          });
-          print(response);
-        },
+        onPressed: () async {},
       ),
     );
   }
