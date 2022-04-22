33f36199ae9cc89ee01576839283ff61f50e69b3
aittataabderrahim <aittata.abderrahim@gmail.com>
2022-04-22 21:44:41 +0000

    commit

--- lib/app/modules/admin/bindings/admin_binding.dart
+++ lib/app/modules/admin/bindings/admin_binding.dart
@@ -5,8 +5,6 @@ import '../controllers/admin_controller.dart';
 class AdminBinding extends Bindings {
   @override
   void dependencies() {
-    Get.lazyPut<AdminController>(
-      () => AdminController(),
-    );
+    Get.lazyPut<AdminController>(() => AdminController());
   }
 }
--- lib/app/modules/admin/controllers/admin_controller.dart
+++ lib/app/modules/admin/controllers/admin_controller.dart
@@ -1,20 +1,3 @@
 import 'package:get/get.dart';
 
-class AdminController extends GetxController {
-  //TODO: Implement AdminController
-
-  final count = 0.obs;
-  @override
-  void onInit() {
-    super.onInit();
-  }
-
-  @override
-  void onReady() {
-    super.onReady();
-  }
-
-  @override
-  void onClose() {}
-  void increment() => count.value++;
-}
+class AdminController extends GetxController {}
