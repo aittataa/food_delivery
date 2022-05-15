aa3cabb6b76edcd86a0a48702d07b8d6e388f57c
aittataabderrahim <aittata.abderrahim@gmail.com>
2022-05-15 23:03:29 +0100

    commit

--- lib/app/modules/admin/views/admin_view.dart
+++ lib/app/modules/admin/views/admin_view.dart
@@ -51,25 +51,7 @@ class _AdminViewState extends State<AdminView> {
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.stretch,
             children: [
-              Container(
-                width: double.infinity,
-                padding: EdgeInsets.symmetric(vertical: 25, horizontal: 20),
-                decoration: BoxDecoration(color: AppTheme.secondaryBackColor),
-                child: ListTile(
-                  contentPadding: EdgeInsets.zero,
-                  leading: Image.asset(AppMessage.appIconRound),
-                  trailing: Image.asset(AppMessage.appLogo),
-                  title: Text(
-                    AppMessage.appTitle,
-                    textAlign: TextAlign.center,
-                    style: TextStyle(
-                      color: AppTheme.primaryTextColor,
-                      fontWeight: FontWeight.bold,
-                      fontSize: 25,
-                    ),
-                  ),
-                ),
-              ),
+              Header(),
               Expanded(
                 child: ListView(
                   shrinkWrap: true,
@@ -248,6 +230,33 @@ class _AdminViewState extends State<AdminView> {
           ),
         );
       }),
+    );
+  }
+}
+
+class Header extends StatelessWidget {
+  const Header({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      padding: EdgeInsets.symmetric(vertical: 25, horizontal: 20),
+      decoration: BoxDecoration(color: AppTheme.secondaryBackColor),
+      child: ListTile(
+        contentPadding: EdgeInsets.zero,
+        leading: Image.asset(AppMessage.appIconRound),
+        trailing: Image.asset(AppMessage.appLogo),
+        title: Text(
+          AppMessage.appTitle,
+          textAlign: TextAlign.center,
+          style: TextStyle(
+            color: AppTheme.primaryTextColor,
+            fontWeight: FontWeight.bold,
+            fontSize: 25,
+          ),
+        ),
+      ),
     );
   }
 }
